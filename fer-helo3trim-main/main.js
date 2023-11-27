b3f20e2771dfc14cd5f9fd715cb1c47f75200433
fernanda.weber <108464103+ruivaflw@users.noreply.github.com>
2023-11-27 10:19:06 -0300

    Delete fer-helo3trim-main directory

--- fer-helo3trim-main/main.js
+++
@@ -1,6 +0,0 @@
-function tocaSomNatureza1(){
-    document.querySelector("#som_tecla_natureza1").play();
-}
-const listadeTeclas = document.querySelectorAll(".tecla");
-
-listadeTeclas[0].onclick = tocaSomNatureza1;
